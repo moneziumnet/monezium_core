c01972b3ee229a3e6eac9bf55d72bd39ccab163a
Rashedul Hasan <rashed.0123@gmail.com>
2022-06-07 00:23:02 +0600

    changed merchant panel like as admin
    changed merchant panel like as admin

--- project/resources/views/layouts/merchant.blade.php
+++ project/resources/views/layouts/merchant.blade.php
@@ -1,142 +1,193 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{$gs->title.'-'}}@yield('title')</title>
-    <link rel="shortcut icon" href="{{getPhoto($gs->favicon)}}">
-    <link rel="stylesheet" href="{{asset('assets/merchant/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/merchant/css/font-awsome.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/merchant/css/selectric.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/merchant/css/jquery-ui.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/merchant/css/select2.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/merchant/css/tagify.css') }}">
-    <link rel="stylesheet" href="{{asset('assets/merchant/css/summernote.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/merchant/css/bootstrap-iconpicker.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/merchant/css/colorpicker.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/merchant/css/style.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/merchant/css/components.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/merchant/css/custom.css')}}">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="{{ $gs->title }}">
+  <meta name="author" content="{{url('/')}}">
+  <link href="{{ asset('assets/images/'.$gs->favicon) }}" rel="icon">
+  <title>{{ $gs->title }}</title>
+  <link href="{{ asset('assets/admin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('assets/admin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('assets/admin/css/toastr.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/admin/css/select2.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/admin/css/tagify.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('assets/admin/css/summernote.css')}}">
+  <link href="{{ asset('assets/admin/css/bootstrap-colorpicker.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/admin/css/bootstrap-iconpicker.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/admin/css/color-picker.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('assets/front/css/toastr.min.css')}}">
+  <link href="{{asset('assets/admin/css/plugin.css')}}" rel="stylesheet" />
+  <link href="{{ asset('assets/admin/css/ruang-admin.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/admin/css/custom.css') }}" rel="stylesheet">
 
-    @stack('style')
+  @yield('styles')
+
 </head>
-<body>
-  
-<div id="app">
-    <div class="main-wrapper">
-      <div class="navbar-bg"></div>
-         @include('merchant.partials.topbar')
-      <div class="main-sidebar">
-         @include('merchant.partials.sidebar')
+
+<body id="page-top">
+    @if ($gs->is_admin_loader==1)
+        <div class="Loader" style="background: url({{asset('assets/images/'.$gs->admin_loader)}}) no-repeat scroll center center #FFF;"></div>
+    @endif
+  <div id="wrapper">
+    <!-- Sidebar -->
+    <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('merchant.dashboard')}}">
+        <div class="sidebar-brand-icon">
+          <img src="{{ asset('assets/images/'.$gs->logo) }}">
+        </div>
+      </a>
+
+      <li class="nav-item">
+        <a class="nav-link" href="{{route('merchant.dashboard')}}">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>{{ __('Dashboard') }}</span></a>
+      </li>
+
+      @include('merchant.partials.sidebar')
+
+    </ul>
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+        <nav class="navbar navbar-expand navbar-light bg-navbar topbar mb-4 static-top">
+          <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown no-arrow mx-1">
+              <a class="nav-link pr-0" target="_blank" href="{{url('/')}}">
+                  <i class="fas fa-globe fa-fw"></i>
+              </a>
+            </li>
+
+
+            <div class="topbar-divider d-none d-sm-block"></div>
+            <li class="nav-item dropdown no-arrow">
+              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <img class="img-profile rounded-circle" src="{{getPhoto(merchant()->photo)}}" style="max-width: 60px">
+                <span class="ml-2 d-none d-lg-inline text-white small">{{merchant()->name}}</span>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+
+                <a class="dropdown-item" href="{{route('merchant.profile.setting')}}">
+                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                    {{ __('Profile Setting') }}
+                  </a>
+                
+                <a class="dropdown-item" href="{{route('merchant.change.password')}}">
+                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                  {{ __('Change Password') }}
+                </a>
+                <a class="dropdown-item" href="{{route('merchant.two.step')}}">
+                    <i class="fas fa-lock fa-sm fa-fw mr-2 text-gray-400"></i>
+                    {{ __('Two Step Security') }}
+                  </a>
+                <a class="dropdown-item" href="{{route('merchant.ticket.index')}}">
+                  <i class="fas fa-ticket-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                  {{ __('Support Ticket') }}
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="{{route('merchant.logout')}}">
+                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                  {{ __('Logout') }}
+                </a>
+              </div>
+            </li>
+          </ul>
+        </nav>
+        <!-- Topbar -->
+
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+
+            @yield('content')
+
+        </div>
+        <!---Container Fluid-->
       </div>
 
-      <!-- Main Content -->
-      <div class="main-content">
-        @yield('breadcrumb')
-        @include('merchant.partials.kyc_info')
-        @yield('content')
-      </div>
-      
     </div>
   </div>
 
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
 
 
+  <script type="text/javascript">
+    'use strict';
+    var form_error   = "{{ __('Please fill all the required fields') }}";
+    var mainurl = "{{ url('/') }}";
+    var admin_loader = {{ $gs->is_admin_loader }};
+
+  </script>
+
+  <script src="{{ asset('assets/admin/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <script src="{{asset('assets/admin/js/plugin.js')}}"></script>
+  <script src="{{ asset('assets/admin/js/chart.min.js') }}"></script>
+  <script src="{{ asset('assets/admin/js/toastr.js') }}"></script>
+  <script src="{{ asset('assets/admin/js/bootstrap-colorpicker.js') }}"></script>
+  <script src="{{ asset('assets/admin/js/bootstrap-iconpicker.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/admin/js/colorpicker.js') }}"></script>
+  <script src="{{ asset('assets/admin/js/select2.min.js') }}"></script>
+  <script src="{{ asset('assets/admin/js/tagify.js') }}"></script>
+  <script src="{{asset('assets/admin/js/summernote.js')}}"></script>
+  <script src="{{ asset('assets/admin/js/sortable.js') }}"></script>
+  <script src="{{ asset('assets/admin/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/admin/js/bootstrap-datepicker.min.js') }}"></script>
+  <script src="{{asset('assets/front/js/toastr.min.js')}}"></script>
+  <script src="{{ asset('assets/admin/js/ruang-admin.js') }}"></script>
+
+  <script>
+    'use strict';
   
-  <!-- Modal -->
-  <div class="modal fade" id="cleardb" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-    <form action="" method="post">
-      @csrf
+    @if(Session::has('message'))
+      toastr.options =
+      {
+        "closeButton" : true,
+        "progressBar" : true
+      }
+      toastr.success("{{ session('message') }}");
+    @endif
     
-      <div class="modal-content">
-          <div class="modal-header">
-              <h5 class="modal-title">@changeLang('Clear Database')</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        <div class="modal-body">
-          <div class="container-fluid">
-              <p>@changeLang('Are You Sure To Clear Database')</p>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">@changeLang('Close')</button>
-          <button type="submit" class="btn btn-danger">@changeLang('Clear Database')</button>
-        </div>
-      </div>
-      </form>
-    </div>
-  </div>
-
-
-
-    @include('notify.alert')
-    <script src="{{asset('assets/merchant/js/jquery.min.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/popper.min.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/jquery-ui.min.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/nicescroll.min.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/summernote.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/select2.min.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/tagify.js') }}"></script>
-    <script src="{{asset('assets/merchant/js/sortable.js') }}"></script>
-    <script src="{{asset('assets/merchant/js/moment-a.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/stisla.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/bootstrap-iconpicker.bundle.min.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/colorpicker.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/jquery.uploadpreview.min.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/chart.min.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/scripts.js')}}"></script>
-    <script src="{{asset('assets/merchant/js/custom.js')}}"></script>
-
-
-    <script>
-
-      var form_error   = "{{ __('Please fill all the required fields') }}";
-      var mainurl = "{{ url('/') }}";
-      var lang  = {
-            'new': '{{ __('ADD NEW') }}',
-            'edit': '{{ __('EDIT') }}',
-            'details': '{{ __('DETAILS') }}',
-            'update': '{{ __('Status Updated Successfully.') }}',
-            'sss': '{{ __('Success !') }}',
-            'active': '{{ __('Activated') }}',
-            'deactive': '{{ __('Deactivated') }}',
-            'loading': '{{ __('Please wait Data Processing...') }}',
-            'submit': '{{ __('Submit') }}',
-            'enter_name': '{{ __('Enter Name') }}',
-            'enter_price': '{{ __('Enter Price') }}',
-            'per_day': '{{ __('Per Day') }}',
-            'per_month': '{{ __('Per Month') }}',
-            'per_year': '{{ __('Per Year') }}',
-            'one_time': '{{ __('One Time') }}',
-            'enter_title': '{{ __('Enter Title') }}',
-            'enter_content': '{{ __('Enter Content') }}',
-            'extra_price_name' : '{{__('Enter Name')}}',
-            'extra_price' : '{{__('Enter Price')}}',
-            'policy_title' : '{{__('Enter Title')}}',
-            'policy_content' : '{{__('Enter Content')}}',
-        };
-  
-    </script>
-
-
-    <script>
-      $(function(){
-        'use strict'
-        $('.reason').on('click',function(){
-          $('#modal-reason').find('.reason-text').val($(this).data('reason'))
-          $('#modal-reason').modal('show')
-        })
-
-      })
+    @if(Session::has('error'))
+      toastr.options =
+      {
+        "closeButton" : true,
+        "progressBar" : true
+      }
+      toastr.error("{{ session('error') }}");
+    @endif
     
-    $('.summernote').summernote()
-    </script>
-    @stack('script')
+    @if(Session::has('info'))
+      toastr.options =
+      {
+        "closeButton" : true,
+        "progressBar" : true
+      }
+      toastr.info("{{ session('info') }}");
+    @endif
     
+    @if(Session::has('warning'))
+      toastr.options =
+      {
+        "closeButton" : true,
+        "progressBar" : true
+      }
+      toastr.warning("{{ session('warning') }}");
+    @endif
+  </script>
+
+  @yield('scripts')
+
 </body>
+
 </html>
--- project/resources/views/merchant/partials/sidebar.blade.php
+++ project/resources/views/merchant/partials/sidebar.blade.php
@@ -1,46 +1,56 @@
-<aside id="sidebar-wrapper">
-  <ul class="sidebar-menu">
-      <li class="menu-header">@lang('Dashboard')</li>
-      <li class="nav-item {{menu('merchant.dashboard')}}">
-        <a href="{{route('merchant.dashboard')}}" class="nav-link"><i class="fas fa-fire"></i><span>@lang('Dashboard')</span></a>
-      </li>
-      
-      <li class="nav-item {{menu('merchant.qr')}}">
-        <a href="{{route('merchant.qr')}}" class="nav-link"><i class="fas fa-qrcode"></i><span>@lang('QR Code')</span></a>
-      </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{route('merchant.qr')}}">
+      <i class="fas fa-qrcode"></i>
+      <span>{{ __('QR Code') }}</span>
+    </a>
+  </li>
 
-      <li class="nav-item {{menu('merchant.api.key.form')}}">
-        <a href="{{route('merchant.api.key.form')}}" class="nav-link"><i class="fas fa-key"></i><span>@lang('API Access Key')</span></a>
-      </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{route('merchant.api.key.form')}}">
+      <i class="fas fa-key"></i>
+      <span>{{ __('API Access Key') }}</span>
+    </a>
+  </li>
 
-      <li class="nav-item {{menu('merchant.transactions')}}">
-        <a href="{{route('merchant.transactions')}}" class="nav-link"><i class="fas fa-exchange-alt"></i><span>@lang('Transactions')</span></a>
-      </li> 
+  <li class="nav-item">
+    <a class="nav-link" href="{{route('merchant.transactions')}}">
+      <i class="fas fa-exchange-alt"></i>
+      <span>{{ __('Transactions') }}</span>
+    </a>
+  </li>
 
-      <li class="menu-header">@lang('Withdraw')</li>
-      <li class="nav-item dropdown {{menu('merchant.withdraw*')}}">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-university"></i> <span>@lang('Withdraw')</span></a>
-        <ul class="dropdown-menu">
-          <li class="{{menu('merchant.withdraw.form')}}"><a class="nav-link" href="{{route('merchant.withdraw.form')}}">@lang('Withdraw Money')</a></li>
-          <li class="{{menu('merchant.withdraw.history')}}"><a class="nav-link" href="{{route('merchant.withdraw.history')}}">@lang('Withdraw History')</a></li>
-         
-        </ul>
-      </li>
-      <li class="menu-header">@lang('Setting')</li>
-      <li class="nav-item {{menu('merchant.profile.setting')}}">
-        <a href="{{route('merchant.profile.setting')}}" class="nav-link"><i class="far fa-user"></i><span>@lang('Profile Setting')</span></a>
-      </li>
-      <li class="nav-item {{menu('merchant.change.password')}}">
-        <a href="{{route('merchant.change.password')}}" class="nav-link"><i class="fas fa-key"></i><span>@lang('Change Password')</span></a>
-      </li>
-      <li class="nav-item {{menu('merchant.two.step')}}">
-        <a href="{{route('merchant.two.step')}}" class="nav-link"><i class="fas fa-lock"></i><span>@lang('Two Step Security')</span></a>
-      </li>
-      <li class="nav-item {{menu('merchant.ticket.index')}}">
-        <a href="{{route('merchant.ticket.index')}}" class="nav-link"><i class="fas fa-ticket-alt"></i><span>@lang('Support Ticket')</span></a>
-      </li>
-      <li class="nav-item {{menu('merchant.logout')}}">
-        <a href="{{route('merchant.logout')}}" class="nav-link"><i class="fas fa-sign-out-alt"></i><span>@lang('Log Out')</span></a>
-      </li>
-    </ul>
-</aside>
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#withdraw" aria-expanded="true" aria-controls="collapseTable">
+      <i class="fas fa-university"></i>
+    <span>{{ __('Withdraw') }}</span>
+  </a>
+    <div id="withdraw" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="{{route('merchant.withdraw.form')}}">{{ __('Withdraw Money') }}</a>
+        <a class="collapse-item" href="{{route('merchant.withdraw.history')}}">{{ __('Withdraw History') }}</a>
+      </div>
+    </div>
+  </li>
+  
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#setting" aria-expanded="true" aria-controls="collapseTable">
+      <i class="fas fa-user"></i>
+    <span>{{ __('Setting') }}</span>
+  </a>
+    <div id="setting" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="{{route('merchant.profile.setting')}}">{{ __('Profile Setting') }}</a>
+        <a class="collapse-item" href="{{route('merchant.change.password')}}">{{ __('Change Password') }}</a>
+        <a class="collapse-item" href="{{route('merchant.two.step')}}">{{ __('Two Step Security') }}</a>
+        <a class="collapse-item" href="{{route('merchant.ticket.index')}}">{{ __('Support Ticket') }}</a>
+      </div>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{route('merchant.logout')}}">
+      <i class="fas fa-sign-out-alt"></i>
+      <span>{{ __('Log Out') }}</span>
+    </a>
+  </li>
+  
+  
