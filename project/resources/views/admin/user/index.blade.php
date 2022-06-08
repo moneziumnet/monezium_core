d3a4bb1a2f4ea58f293969e36ec5eea2c6860ef6
aca dev <violetfocus0618@gmail.com>
2022-06-08 16:28:53 +0200

    disable user create button on admin

--- project/resources/views/admin/user/index.blade.php
+++ project/resources/views/admin/user/index.blade.php
@@ -183,13 +183,13 @@
                     processing: '<img src="{{asset('assets/images/'.$gs->admin_loader)}}">'
                 }
             });
-         $(function() {
-            $(".btn-area").append('<div class="col-sm-12 col-md-4 pr-3 text-right">'+
-                '<a class="btn btn-primary" href="{{route('admin.user.create')}}">'+
-            '<i class="fas fa-plus"></i> Add New Institution'+
-            '</a>'+
-            '</div>');
-        });
+        //  $(function() {
+        //     $(".btn-area").append('<div class="col-sm-12 col-md-4 pr-3 text-right">'+
+        //         '<a class="btn btn-primary" href="{{route('admin.user.create')}}">'+
+        //     '<i class="fas fa-plus"></i> Add New Institution'+
+        //     '</a>'+
+        //     '</div>');
+        // });
 
 
 </script>
