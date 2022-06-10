e6d4eea96630ca1e8a3fd54dc69fe43aa82bffaf
aca dev <violetfocus0618@gmail.com>
2022-06-10 17:29:03 +0200

    Update 2022_06_03_143046_create_admins_table.php

--- project/database/migrations/tenant/2022_06_03_143046_create_admins_table.php
+++ project/database/migrations/tenant/2022_06_03_143046_create_admins_table.php
@@ -20,6 +20,19 @@ class CreateAdminsTable extends Migration
             $table->string('phone', 191);
             $table->tinyInteger('role_id')->default(0);
             $table->string('photo', 191)->nullable();
+            
+            $table->string('zip', 191)->nullable();
+            $table->string('city', 191)->nullable();
+            $table->string('address', 191)->nullable();
+            $table->string('country_id')->nullable();
+            $table->string('vat', 191)->nullable();
+            // $table->string('full_name', 191)->nullable();
+            // $table->timestamp('birth_date')->nullable();
+            // $table->string('persional_code', 191)->nullable();
+            // $table->string('passport_number', 191)->nullable();
+            // $table->string('issue_date', 191)->nullable();
+            // $table->string('expire_date', 191)->nullable();
+            // $table->string('authority', 191)->nullable();
             $table->string('password', 191);
             $table->tinyInteger('status')->default(1);
             $table->rememberToken();
