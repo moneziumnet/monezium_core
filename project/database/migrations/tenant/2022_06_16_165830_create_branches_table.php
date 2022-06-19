2d0241f19d7c7f92a127c48f39bac52e70d3a5b3
aca dev <violetfocus0618@gmail.com>
2022-06-19 15:16:43 +0200

    Update 2022_06_16_165830_create_branches_table.php

--- project/database/migrations/tenant/2022_06_16_165830_create_branches_table.php
+++ project/database/migrations/tenant/2022_06_16_165830_create_branches_table.php
@@ -14,7 +14,7 @@ class CreateBranchesTable extends Migration
     public function up()
     {
         Schema::create('branches', function (Blueprint $table) {
-            $table->increments('id')->primary();
+            $table->increments('id');
             $table->string('name', 255);
             $table->timestamps();
         });
