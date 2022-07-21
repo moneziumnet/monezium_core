12d53d11d4ab42875a14e1a2aaca0ed665bc4ffa
Rashedul Hasan <rashed.0123@gmail.com>
2022-07-21 17:51:05 +0600

    RESTFull API (Voucher) All Voucher Done
    RESTFull API (Voucher) All Voucher Done

--- project/app/Http/Controllers/API/UserController.php
+++ project/app/Http/Controllers/API/UserController.php
@@ -1002,13 +1002,87 @@ class UserController extends Controller
             return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'Something invalid.']);
         }
     }
+
+    public function createvoucher(Request $request)
+    {
+        try{
+            $user_id = UserApiCred::where('access_key', $request->access_key)->first()->user_id;
+            $request->validate([
+                'wallet_id' => 'required|integer',
+                'amount' => 'required|numeric|gt:0'
+            ]);
+
+
+        $wallet = Wallet::where('id',$request->wallet_id)->where('user_type',1)->where('user_id',$user_id)->first();
+        if(!$wallet) return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'Wallet not found']);
+        
+        $charge = charge('create-voucher');
+        $rate = $wallet->currency->rate;
+
+        if($request->amount <  $charge->minimum * $rate || $request->amount >  $charge->maximum * $rate){
+            return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'Please follow the limit']);
+        }
+
+        $finalCharge = chargeCalc($charge,$request->amount,$rate);
+        $finalAmount = numFormat($request->amount + $finalCharge);
+
+        $commission  = ($request->amount * $charge->commission)/100;
+      
+        $voucher = new Voucher();
+        $voucher->user_id = $user_id;
+        $voucher->currency_id = $wallet->currency_id;
+        $voucher->amount = $request->amount;
+        $voucher->code = randNum(10).'-'.randNum(10);
+        $voucher->save();
+        $userBalance = user_wallet_balance($user_id, $wallet->currency_id);
+        if($finalAmount > $userBalance) return response()->json(['status' => '401', 'error_code' => $finalAmount, 'message' => 'Wallet has insufficient balance']);
+        
+        user_wallet_decrement($user_id,$wallet->currency_id,$finalAmount);
+        // $wallet->balance -=  $finalAmount;
+        // $wallet->save();
+
+        $trnx              = new Transaction();
+        $trnx->trnx        = str_rand();
+        $trnx->user_id     = $user_id;
+        $trnx->user_type   = 1;
+        $trnx->currency_id = $wallet->currency->id;
+        $trnx->amount      = $finalAmount;
+        $trnx->charge      = $finalCharge;
+        $trnx->remark      = 'create_voucher';
+        $trnx->type        = '-';
+        $trnx->details     = trans('Voucher created');
+        $trnx->save();
+
+        user_wallet_increment($user_id,$wallet->currency_id,$commission);
+        // $wallet->balance +=  $commission;
+        // $wallet->save();
+
+        $commissionTrnx              = new Transaction();
+        $commissionTrnx->trnx        = $trnx->trnx;
+        $commissionTrnx->user_id     = $user_id;
+        $commissionTrnx->user_type   = 1;
+        $commissionTrnx->currency_id = $wallet->currency->id;
+        $commissionTrnx->amount      = $commission;
+        $commissionTrnx->charge      = 0;
+        $commissionTrnx->remark      = 'voucher_commission';
+        $commissionTrnx->type        = '+';
+        $commissionTrnx->details     = trans('Voucher commission');
+        $commissionTrnx->save();
+        $data['vouchers']          = Voucher::with('currency')->whereUserId($user_id)->orderby('id','desc')->paginate(10);
+        return response()->json(['status' => '200', 'error_code' => '0', 'message' => 'Voucher has been created successfully', 'data' => $data]);
+            
+        }catch(\Throwable $th){
+            return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'Something invalid.']);
+        }
+    }
     
     public function reedemvoucher(Request $request)
     {
         try{
             $user_id = UserApiCred::where('access_key', $request->access_key)->first()->user_id;
             $request->validate([
-                'code'          => 'required'
+                'code'          => 'required',
+               
             ],
             [
                 'code.required' => 'Voucher is required'
@@ -1016,13 +1090,50 @@ class UserController extends Controller
             
             $user = User::whereId($user_id)->first();
 
+            $voucher = Voucher::where('code',$request->code)->where('status',0)->first();
+      
+            if(!$voucher){
+                return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'Invalid voucher code']);
+            }
+
+            if( $voucher->user_id == $user_id){
+                return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'Can\'t reedem your own voucher']);
+            }
+
+            $wallet = Wallet::where('currency_id',$voucher->currency_id)->where('user_id',$user_id)->first();
+            if(!$wallet){
+                $wallet = Wallet::create([
+                    'user_id' => $user_id,
+                    'user_type' => 1,
+                    'currency_id' => $voucher->currency_id,
+                    'balance'   => 0
+                ]);
+            }
+
+            $wallet->balance += $voucher->amount;
+            $wallet->update();
+
+            $trnx              = new Transaction();
+            $trnx->trnx        = str_rand();
+            $trnx->user_id     = $user_id;
+            $trnx->user_type   = 1;
+            $trnx->currency_id = $wallet->currency->id;
+            $trnx->amount      = $voucher->amount;
+            $trnx->charge      = 0;
+            $trnx->type        = '+';
+            $trnx->remark      = 'reedem_voucher';
+            $trnx->details     = trans('Voucher reedemed');
+            $trnx->save();
+
+            $voucher->status = 1;
+            $voucher->reedemed_by = $user_id;
+            $voucher->update();
+            return response()->json(['status' => '200', 'error_code' => '0', 'message' => 'Voucher reedemed successfully']);
             
 
-            $data['vouchers']          = Voucher::with('currency')->whereUserId($user_id)->orderby('id','desc')->paginate(10);
-            return response()->json(['status' => '200', 'error_code' => '0', 'message' => 'success', 'data' => $data]);
-        }catch(\Throwable $th){
-            return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'Something invalid.']);
-        }
+        } catch (\Throwable $th) {
+                return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'Something invalid.']);
+           }
     }
     
     public function reedemedhistory(Request $request)
