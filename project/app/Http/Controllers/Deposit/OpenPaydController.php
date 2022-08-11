3e77f233dcb0eae19bee78606229140b2398f68d
WHdev711 <appc31058@gmail.com>
2022-08-11 11:44:44 +0300

    Validate bank api

--- project/app/Http/Controllers/Deposit/OpenPaydController.php
+++ project/app/Http/Controllers/Deposit/OpenPaydController.php
@@ -24,41 +24,56 @@ class OpenPaydController extends Controller
             return redirect()->back()->with(array('warning' => 'This bank account already exists.'));
 
         }
+        try {
+            $response = $client->request('POST', 'https://sandbox.openpayd.com/api/oauth/token?grant_type=client_credentials', [
+                'headers' => [
+                   'Accept'=> 'application/json',
+                  'Authorization' => 'Basic '.$bankgateway->information->Auth,
+                  'Content-Type' => 'application/x-www-form-urlencoded',
+                ],
+              ]);
+            $res_body = json_decode($response->getBody());
+            $auth_token = $res_body->access_token;
+            $accounter_id = $res_body->accountHolderId;
+        } catch (\Throwable $th) {
+             return redirect()->back()->with(array('warning' => 'Some Value is incorrect'));
+        }
+        try {
+            $currency = Currency::whereId($request->currency)->first();
+            $response = $client->request('POST', 'https://sandbox.openpayd.com/api/accounts', [
+                'body' => '{"currency":"'.$currency->code.'","friendlyName":"Billing Account('.$currency->code.')"}',
+                'headers' => [
+                  'Accept' => 'application/json',
+                  'Authorization' => 'Bearer '.$auth_token,
+                  'Content-Type' => 'application/json',
+                  'x-account-holder-id' => $accounter_id,
+                ],
+            ]);
+            $res_body = json_decode($response->getBody());
+            $internal_id = $res_body->internalAccountId;
+        } catch (\Throwable $th) {
+             return redirect()->back()->with(array('warning' => 'Some Value is incorrect'));
+        }
 
-        $response = $client->request('POST', 'https://sandbox.openpayd.com/api/oauth/token?grant_type=client_credentials', [
-            'headers' => [
-               'Accept'=> 'application/json',
-              'Authorization' => 'Basic '.$bankgateway->information->Auth,
-              'Content-Type' => 'application/x-www-form-urlencoded',
-            ],
-          ]);
-        $res_body = json_decode($response->getBody());
-        $auth_token = $res_body->access_token;
-        $accounter_id = $res_body->accountHolderId;
-        $currency = Currency::whereId($request->currency)->first();
-        $response = $client->request('POST', 'https://sandbox.openpayd.com/api/accounts', [
-            'body' => '{"currency":"'.$currency->code.'","friendlyName":"Billing Account('.$currency->code.')"}',
-            'headers' => [
-              'Accept' => 'application/json',
-              'Authorization' => 'Bearer '.$auth_token,
-              'Content-Type' => 'application/json',
-              'x-account-holder-id' => $accounter_id,
-            ],
-        ]);
-        $res_body = json_decode($response->getBody());
-        $internal_id = $res_body->internalAccountId;
-        $response = $client->request('GET', 'https://sandbox.openpayd.com/api/bank-accounts?internalAccountId='.$internal_id, [
-            'headers' => [
-              'Accept' => 'application/json',
-              'Authorization' => 'Bearer '.$auth_token,
-              'Content-Type' => 'application/json',
-              'x-account-holder-id' => $accounter_id,
-            ],
-        ]);
-        $res_body = json_decode($response->getBody())[0];
+        try {
+            $response = $client->request('GET', 'https://sandbox.openpayd.com/api/bank-accounts?internalAccountId='.$internal_id, [
+                'headers' => [
+                  'Accept' => 'application/json',
+                  'Authorization' => 'Bearer '.$auth_token,
+                  'Content-Type' => 'application/json',
+                  'x-account-holder-id' => $accounter_id,
+                ],
+            ]);
+            $res_body = json_decode($response->getBody())[0];
 
-        $bic_swift = $res_body->bic;
-        $iban = $res_body->iban;
+            $bic_swift = $res_body->bic;
+            $iban = $res_body->iban;
+        } catch (\Throwable $th) {
+             return redirect()->back()->with(array('warning' => 'Some Value is incorrect'));
+        }
+
+
+
         $data = New BankAccount();
         $data->user_id = $request->user;
         $data->subbank_id = $request->subbank;
--- project/app/Http/Controllers/Deposit/RailsBankController.php
+++ project/app/Http/Controllers/Deposit/RailsBankController.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use GuzzleHttp\Client;
+use Auth;
 
 class RailsBankController extends Controller
 {
@@ -21,50 +22,67 @@ class RailsBankController extends Controller
         $bankaccount = BankAccount::where('user_id', $request->user)->where('subbank_id', $request->subbank)->where('currency_id', $request->currency)->first();
         if ($bankaccount){
             return redirect()->back()->with(array('warning' => 'This bank account already exists.'));
+        }
+        try {
+            $response = $client->request('POST', 'https://play.railsbank.com/v1/customer/endusers', [
+                'body' => '{
+                    "person": {
+                      "name": "'.$user->name.'",
+                      "email": "'.$user->email.'",
+                      "address": { "address_iso_country": "US" }
+                    }
+                }',
+                'headers' => [
+                    'Accept'=> 'application/json',
+                    'Authorization' => 'API-Key '.$bankgateway->information->API_Key,
+                    'Content-Type' => 'application/json',
+                ],
+            ]);
+            $enduser = json_decode($response->getBody())->enduser_id;
+        } catch (\Throwable $ex) {
+            //throw $th;
+            return redirect()->back()->with(array('warning' => 'Some Value is incorrect'));
+        }
+
+        try {
+            $response = $client->request('POST', 'https://play.railsbank.com/v1/customer/ledgers', [
+                'body' => '{
+                    "holder_id": "'.$enduser.'",
+                    "partner_product": "ExampleBank-EUR-1",
+                    "asset_class": "currency",
+                    "asset_type": "eur",
+                    "ledger-type": "ledger-type-single-user",
+                    "ledger-who-owns-assets": "ledger-assets-owned-by-me",
+                    "ledger-primary-use-types": ["ledger-primary-use-types-payments"],
+                    "ledger-t-and-cs-country-of-jurisdiction": "US"
+                  }',
+                'headers' => [
+                    'Accept'=> 'application/json',
+                    'Authorization' => 'API-Key '.$bankgateway->information->API_Key,
+                    'Content-Type' => 'application/json',
+                ],
+            ]);
+            $ledger = json_decode($response->getBody())->ledger_id;
+        } catch (\Throwable $ex) {
+            return redirect()->back()->with(array('warning' => 'Some Value is incorrect'));
+        }
+
+        try {
+            $response = $client->request('GET', 'https://play.railsbank.com/v1/customer/ledgers/'.$ledger.'/wait', [
+                'headers' => [
+                    'Accept'=> 'application/json',
+                    'Authorization' => 'API-Key '.$bankgateway->information->API_Key,
+                    'Content-Type' => 'application/json',
+                ],
+            ]);
+            $bic_swift = json_decode($response->getBody())->bic_swift;
+            $iban = json_decode($response->getBody())->iban;
+        } catch (\Throwable $ex) {
+            //throw $th;
+            return redirect()->back()->with(array('warning' => 'Some Value is incorrect'));
 
         }
-        $response = $client->request('POST', 'https://play.railsbank.com/v1/customer/endusers', [
-            'body' => '{
-                "person": {
-                  "name": "'.$user->name.'",
-                  "email": "'.$user->email.'",
-                  "address": { "address_iso_country": "US" }
-                }
-            }',
-            'headers' => [
-                'Accept'=> 'application/json',
-                'Authorization' => 'API-Key '.$bankgateway->information->API_Key,
-                'Content-Type' => 'application/json',
-            ],
-        ]);
-        $enduser = json_decode($response->getBody())->enduser_id;
-        $response = $client->request('POST', 'https://play.railsbank.com/v1/customer/ledgers', [
-            'body' => '{
-                "holder_id": "'.$enduser.'",
-                "partner_product": "ExampleBank-EUR-1",
-                "asset_class": "currency",
-                "asset_type": "eur",
-                "ledger-type": "ledger-type-single-user",
-                "ledger-who-owns-assets": "ledger-assets-owned-by-me",
-                "ledger-primary-use-types": ["ledger-primary-use-types-payments"],
-                "ledger-t-and-cs-country-of-jurisdiction": "US"
-              }',
-            'headers' => [
-                'Accept'=> 'application/json',
-                'Authorization' => 'API-Key '.$bankgateway->information->API_Key,
-                'Content-Type' => 'application/json',
-            ],
-        ]);
-        $ledger = json_decode($response->getBody())->ledger_id;
-        $response = $client->request('GET', 'https://play.railsbank.com/v1/customer/ledgers/'.$ledger.'/wait', [
-            'headers' => [
-                'Accept'=> 'application/json',
-                'Authorization' => 'API-Key '.$bankgateway->information->API_Key,
-                'Content-Type' => 'application/json',
-            ],
-        ]);
-        $bic_swift = json_decode($response->getBody())->bic_swift;
-        $iban = json_decode($response->getBody())->iban;
+
         $data = New BankAccount();
         $data->user_id = $request->user;
         $data->subbank_id = $request->subbank;
@@ -147,4 +165,56 @@ class RailsBankController extends Controller
 
         // return redirect()->route('user.depositbank.create')->with('success','Deposit amount '.$request->amount.' ('.$currency->code.') successfully!');
     }
+    // public function transfer(Request $request) {
+    //     $user = auth()->user();
+    //     $customer_bank = BankAccount::whereUserId($user->id)->where('subbank_id',$request->bank)->where('currency_id', $request->currency)->first();
+    //     $bankgateway = BankGateway::where('subbank_id', $request->bank)->first();
+
+    //     $response = $client->request('GET','https://play.railsbank.com/v1/customer/ledgers?account_number='.$customer_bank->iban, [
+    //         'headers' => [
+    //             'Accept'=> 'application/json',
+    //             'Authorization' => 'API-Key '.$bankgateway->information->API_Key,
+    //             'Content-Type' => 'application/json',
+    //         ],
+    //     ]);
+    //     $enduser = json_decode($response->getBody())->holder_id;
+    //     $le
+
+        // $response = $client->request('POST','https://play.railsbank.com/v1/customer/beneficiaries', [
+        //     'body' => '{
+        //         "holder_id": "'.$enduser.'",
+        //         "asset_class": "currency",
+        //         "asset_type": "eur",
+        //         "iban": "'.$subbank->iban.'",
+        //         "bic_swift": "'.$subbank->swift.'",
+        //         "person": {
+        //           "name": "'.$subuser->name.'",
+        //           "email": "'.$subuser->email.'",
+        //           "address": { "address_iso_country": "US" }
+        //         }
+        //       }',
+        //     'headers' => [
+        //         'Accept'=> 'application/json',
+        //         'Authorization' => 'API-Key '.$bankgateway->information->API_Key,
+        //         'Content-Type' => 'application/json',
+        //     ],
+        // ]);
+        // $beneficiary = json_decode($response->getBody())->beneficiary_id;
+
+        // $response = $client->request('POST', 'https://play.railsbank.com/v1/customer/transactions', [
+        //     'body' => '{
+        //         "ledger_from_id": "'.$ledger.'",
+        //         "beneficiary_id": "'.$beneficiary.'",
+        //         "payment_type": "payment-type-EU-SEPA-Step2",
+        //         "amount": "'.$request->amount.'"
+        //       }',
+        //     'headers' => [
+        //        'Accept'=> 'application/json',
+        //       'Authorization' => 'API-Key '.$bankgateway->information->API_Key,
+        //       'Content-Type' => 'application/json',
+        //     ],
+        //   ]);
+        // $transaction = json_decode($response->getBody())->transaction_id;
+    // }
+
 }
