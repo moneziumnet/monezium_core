df1a59843ab0460cb96779e3fa3a192fff28a445
WHdev711 <appc31058@gmail.com>
2022-09-01 12:35:32 +0300

    Update OtherBankTransferController.php

--- project/app/Http/Controllers/Admin/OtherBankTransferController.php
+++ project/app/Http/Controllers/Admin/OtherBankTransferController.php
@@ -247,7 +247,7 @@ class OtherBankTransferController extends Controller
             $auth_token = $res_body->access_token;
             $accounter_id = $res_body->accountHolderId;
         } catch (\Throwable $th) {
-             return redirect()->back()->with(array('warning' => 'Some Valuen is incorrect '));
+             return response()->json('Some Valuen is incorrect');
         }
 
 
@@ -265,10 +265,10 @@ class OtherBankTransferController extends Controller
             $account_id = $res_body->id;
             $amount = $res_body->availableBalance->value;
             if ($amount < $data->amount) {
-                return redirect()->back()->with(array('warning' => 'Insufficient Balance.'));
+                return response()->json(('Some Valuen is incorrect'));
             }
         } catch (\Throwable $th) {
-             return redirect()->back()->with(array('warning' => 'Some Value is incorrect'));
+             return response()->json(('Some Valuen is incorrect'));
         }
 
         try {
@@ -288,7 +288,7 @@ class OtherBankTransferController extends Controller
             $res_body = json_decode($response->getBody());
             $beneficiary_id = $res_body->id;
         } catch (\Throwable $th) {
-             return redirect()->back()->with(array('warning' => 'Some Value is incorrect'));
+             return response()->json(('Some Valuen is incorrect'));
         }
 
         try {
@@ -314,7 +314,7 @@ class OtherBankTransferController extends Controller
             $res_body = json_decode($response->getBody());
             $beneficiary_id = $res_body->id;
         } catch (\Throwable $th) {
-             return redirect()->back()->with(array('warning' => 'Some Value is incorrect'));
+             return response()->json(('Some Valuen is incorrect'));
         }
 
         try {
@@ -337,7 +337,7 @@ class OtherBankTransferController extends Controller
             $res_body = json_decode($response->getBody());
             $transaction_id = $res_body->id;
         } catch (\Throwable $th) {
-             return redirect()->back()->with(array('warning' => 'Some Value is incorrect'));
+             return response()->json(('Some Valuen is incorrect'));
         }
 
 
