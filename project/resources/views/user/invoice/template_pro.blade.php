4dd9cc7c26fb262131b9433f398ef46c0cda2b63
WHdev711 <appc31058@gmail.com>
2022-09-20 11:03:28 -0400

    Add tax information to invoice template

--- project/resources/views/user/invoice/template_pro.blade.php
+++ project/resources/views/user/invoice/template_pro.blade.php
@@ -74,16 +74,64 @@
             <th class="text-end" style="width: 1%">{{ __('Amount') }}</th>
         </tr>
     </thead>
+    @php
+        $total = $invoice->final_amount;
+        $arr_tax = array();
+    @endphp
     @foreach ($invoice->items as $k => $value)
         <tr>
             <td class="text-center">{{ ++$k }}</td>
             <td><p class="strong mb-1">{{ $value->name }}</p></td>
             <td class="text-end">{{ $invoice->currency->symbol }}{{ numFormat($value->amount) }}</td>
         </tr>
+        @php
+            $item = DB::table('taxes')->where('id', $value->tax_id)->first();
+            if($item){
+                array_push($arr_tax, array(
+                    "name"=> $item->name,
+                    "rate"=> $item->rate,
+                    "price"=> $item->rate * $value->amount / 100
+                ));
+                $total += $item->rate * $value->amount / 100;
+            }
+        @endphp
     @endforeach
     <tr>
         <td colspan="12" class="text-end fw-bold">
-            {{ __('Total : ' . $invoice->currency->symbol . numFormat($invoice->final_amount)) }}
+            <div class="row">
+                <div class="col-4 offset-6 text-end">
+                    {{ __('Subtotal : ') }}
+                </div>
+                <div class="col-2">
+                    {{ $invoice->currency->symbol . amount($invoice->final_amount, 1, 2) }}
+                </div>
+            </div>
+        </td>
+    </tr>
+    @foreach ($arr_tax as $tax_item)
+    <tr>
+        <td colspan="12" class="text-end fw-bold">
+            <div class="row">
+                <div class="col-4 offset-6 text-end">
+                    {{ $tax_item['name'].' '.$tax_item['rate'].'%' }}
+                </div>
+                <div class="col-2">
+                    {{ $invoice->currency->symbol . amount($tax_item['price'], 1, 2) }}
+                </div>
+            </div>
+        </td>
+    </tr>
+    @endforeach
+    <tr>
+        <td colspan="12" class="text-end fw-bold">
+            <div class="row">
+                <h3 class="col-4 offset-6 text-end">
+                    {{ __('Total') }}
+                </h3>
+                <h3 class="col-2">
+                    {{ $invoice->currency->symbol . amount($total, 1, 2) }}
+                </h3>
+            </div>
         </td>
     </tr>
 </table>
