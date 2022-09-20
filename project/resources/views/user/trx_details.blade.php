6d24cd02081943db014eca6a86b2ef7dc6331871
WHdev711 <appc31058@gmail.com>
2022-09-20 07:57:36 -0400

    Change transaction detail modal style

--- project/resources/views/user/trx_details.blade.php
+++ project/resources/views/user/trx_details.blade.php
@@ -1,13 +1,26 @@
 
-<li class="list-group-item d-flex justify-content-between" style="word-break: break-all;">@lang('Transaction ID')<span style="margin-left: 60px" >{{$transaction->trnx}}</span></li>
-<li class="list-group-item d-flex justify-content-between">@lang('Remark')<span class="badge badge-primary">{{ucwords(str_replace('_',' ',$transaction->remark))}}</span></li>
-<li class="list-group-item d-flex justify-content-between">@lang('Currency')<span class="font-weight-bold">{{$transaction->currency->code}}</span></li>
-<li class="list-group-item d-flex justify-content-between">@lang('Amount')<span class="badge {{$transaction->type == '+' ? 'badge-success':'badge-danger'}}">{{$transaction->type}}{{amount($transaction->amount,$transaction->currency->type,2)}} {{$transaction->currency->code}}</span></li>
-<li class="list-group-item d-flex justify-content-between">@lang('Charge')<span>{{amount($transaction->charge,$transaction->currency->type,2)}} {{$transaction->currency->code}}</span></li>
-<li class="list-group-item d-flex justify-content-between">@lang('Date')<span>{{dateFormat($transaction->created_at,'d M y')}}</span></li>
+<li class="list-group-item d-flex justify-content-between" style="word-break: break-all;">@lang('Transaction ID')
+    <span style="margin-left: 60px" >{{$transaction->trnx}}</span>
+</li>
+<li class="list-group-item d-flex justify-content-between">@lang('Remark')
+    <span class="badge badge-primary">{{ucwords(str_replace('_',' ',$transaction->remark))}}</span>
+</li>
+<li class="list-group-item d-flex justify-content-between">@lang('Currency')
+    <span class="font-weight-bold">{{$transaction->currency->code}}</span>
+</li>
+<li class="list-group-item d-flex justify-content-between">@lang('Amount')
+    <span class="badge {{$transaction->type == '+' ? 'badge-success':'badge-danger'}}">{{$transaction->type}}{{amount($transaction->amount,$transaction->currency->type,2)}} {{$transaction->currency->code}}</span>
+</li>
+<li class="list-group-item d-flex justify-content-between">@lang('Charge')
+    <span>{{amount($transaction->charge,$transaction->currency->type,2)}} {{$transaction->currency->code}}</span>
+</li>
+<li class="list-group-item d-flex justify-content-between">@lang('Date')
+    <span>{{dateFormat($transaction->created_at,'d M y')}}</span>
+</li>
 @if (isset($transaction->data))
     @foreach ( json_decode($transaction->data) as $key => $value)
-        <li class="list-group-item d-flex justify-content-between">@lang(ucwords($key))<span>{{($value)}}</span></li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">@lang(ucwords($key))
+            <div style="width: 50%; word-break:break-all;" class="text-end">{{($value)}}</div>
+        </li>
     @endforeach
-
 @endif
