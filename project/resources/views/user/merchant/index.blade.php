a4c55de37ad35438435b6ced391cacb4f7b1d050
WHdev711 <appc31058@gmail.com>
2022-09-20 03:52:54 -0400

    Disable update of merchant crypto wallet

--- project/resources/views/user/merchant/index.blade.php
+++ project/resources/views/user/merchant/index.blade.php
@@ -64,7 +64,7 @@
                     @foreach ($wallets as $item)
                     @if ($item->currency->type == 2)
                     <div class="col-sm-6 col-md-4 mb-3" >
-                            <a href="javascript:;" data-bs-toggle="modal" data-bs-target="#modal-success{{$item->id}}"  >
+                        <div>
                             <div class="card h-100 card--info-item">
                                 <div class="text-end icon">
                                 <i class="fas ">
@@ -77,34 +77,6 @@
                                 <div class="text-muted">{{ amount($item->balance,$item->currency->type,2) }}  {{$item->currency->code}}</div>
                                 </div>
                             </div>
-                        </a>
-                        </div>
-                    <div class="modal modal-blur fade" id="modal-success{{$item->id}}" tabindex="-1" role="dialog" aria-hidden="true">
-                        <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            <div class="modal-status bg-primary"></div>
-                            <div class="modal-body text-center py-1">
-                            <i  class="fas fa-info-circle fa-3x text-primary mb-2"></i>
-                            <h3>@lang('Edit Wallet Address')</h3>
-                            </div>
-                            <form id="depositbank_gateway" action="{{ route('user.merchant.cryptowallet.update') }}" method="post"  enctype="multipart/form-data">
-                                @csrf
-                                <div class="modal-body">
-                                    <div class="form-group" id="otp_body">
-                                        <label class="form-label required">{{__('Address')}}</label>
-                                        <input name="address" id="address" class="form-control" placeholder="{{__('0x....')}}" type="text" step="any" value="{{ $item->wallet_no }}" required>
-                                    </div>
-                                  <div class="form-group mt-3">
-                                    <input type="hidden" id="wallet_id" name="wallet_id" value="{{$item->id}}">
-                                   </div>
-                                </div>
-
-                                <div class="modal-footer">
-                                    <button type="submit" id="submit-btn" class="btn btn-primary">{{ __('Ok') }}</button>
-                                </div>
-                            </form>
-                        </div>
                         </div>
                     </div>
                     @endif
@@ -146,8 +118,6 @@
                             </div>
                         </div>
                     </div>
-
-
                 </div>
             </div>
         </div>
