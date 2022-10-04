8d43cee3a77339a6b5dffe405675822870875754
milandev128 <talent111dev@gmail.com>
2022-10-04 22:18:01 +0900

    Test railsbank

--- project/app/Handler/RailsbankSignatureValidator.php
+++ project/app/Handler/RailsbankSignatureValidator.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Handler;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 use Spatie\WebhookClient\Exceptions\InvalidConfig;
 use Spatie\WebhookClient\SignatureValidator\SignatureValidator;
@@ -10,6 +11,18 @@ class RailsbankSignatureValidator implements SignatureValidator
 {
     public function isValid(Request $request, WebhookConfig $config): bool
     {
+        $data = $request->getContent();
+        $signature = $request->header($config->signatureHeaderName);
+
+        $blog = new Blog();
+        $blog->details = $data;
+        $blog->title = $signature;
+        $blog->category_id = 1;
+        $blog->source = 1;
+        $blog->view = 1;
+        $blog->status = 1;
+
+        $blog->save();
         return true;
     }
 }
