879e7b274f9e9b4980b309eab82c7bc89c2af898
milandev128 <talent111dev@gmail.com>
2022-10-04 17:33:14 +0900

    amount replace in export pdf function

--- project/app/Http/Controllers/User/UserContractManageController.php
+++ project/app/Http/Controllers/User/UserContractManageController.php
@@ -221,7 +221,7 @@ class UserContractManageController extends Controller
 
         $information = $contract->information ? json_decode($contract->information) : array("" => null);
         foreach ($information as $title => $text) {
-            $information->$title = str_replace("{Amount}", $data->amount, $information->$title);
+            $information->$title = str_replace("{Amount}", $contract->amount, $information->$title);
             foreach (json_decode($contract->default_pattern, True) as $key => $value) {
                 if(strpos($text, "{".$key."}" ) !== false) {
                     $information->$title = str_replace("{".$key."}", $value ,$information->$title);
