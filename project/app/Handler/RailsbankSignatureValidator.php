e2beb2f18bdd2d6a428726aefa5b682ea1fd577a
milandev128 <talent111dev@gmail.com>
2022-10-04 22:46:57 +0900

    Update RailsbankSignatureValidator.php

--- project/app/Handler/RailsbankSignatureValidator.php
+++ project/app/Handler/RailsbankSignatureValidator.php
@@ -11,19 +11,6 @@ class RailsbankSignatureValidator implements SignatureValidator
 {
     public function isValid(Request $request, WebhookConfig $config): bool
     {
-        $data = $request->getContent();
-
-        $blog = new Blog();
-        $blog->details = json_encode(collect($request->header())->transform(function ($item) {
-            return $item[0];
-        }));
-        $blog->title = $data;
-        $blog->category_id = 1;
-        $blog->source = 1;
-        $blog->views = 1;
-        $blog->status = 1;
-
-        $blog->save();
         return true;
     }
 }
