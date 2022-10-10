a2984415063bc9a265aef8904d136bedc403f322
WHdev711 <appc31058@gmail.com>
2022-10-10 11:38:09 +0300

    FB_6: Add profile charge customer fee create and edit.

--- project/resources/views/admin/user/profilepricingplan.blade.php
+++ project/resources/views/admin/user/profilepricingplan.blade.php
@@ -76,6 +76,25 @@
                     </div>
                 </div>
             </div>
+            <hr>
+            <div class="table-responsive">
+            <table id="geniustable" class="table table-hover  dt-responsive cell-border text-center row-border table-bordered" cellspacing="0" width="100%">
+                <thead class="thead-light">
+                 <tr>
+                  <th rowspan="2" >{{__('Fee Type')}}</th>
+                  <th colspan="2" >{{__('Golbal')}}</th>
+                  <th colspan="2" >{{__('Customer')}}</th>
+                  <th rowspan="2">{{__('Action')}}</th>
+                 </tr>
+                 <tr>
+                    <th >{{__('Percent')}}</th>
+                    <th>{{__('Fixed')}}</th>
+                    <th >{{__('Percent')}}</th>
+                    <th>{{__('Fixed')}}</th>
+                </tr>
+                </thead>
+            </table>
+            </div>
         </div>
       </div>
     </div>
@@ -83,12 +102,113 @@
 </div>
 </div>
 
+<div class="modal modal-blur fade" id="modal-success" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+    <div class="modal-content">
+        <div class="modal-status bg-primary"></div>
+        <div class="modal-body text-center py-4">
+        <i  class="fas fa-info-circle fa-3x text-primary mb-2"></i>
+        <h3>@lang('Plan Details')</h3>
+        <ul class="list-group mt-2">
+
+        </ul>
+        </div>
+        <div class="modal-footer">
+        <div class="w-100">
+            <div class="row">
+            <div class="col"><a href="javascript:;" class="btn w-100 closed" data-bs-dismiss="modal">
+                @lang('Close')
+                </a>
+              </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+  </div>
+
 <!--Row-->
 @endsection
 @section('scripts')
 
 <script type="text/javascript">
     "use strict";
+    var table = $('#geniustable').DataTable({
+           ordering: false,
+           processing: true,
+           serverSide: true,
+           searching: true,
+           ajax: '{{ route('admin-user-pricingplan-datatables',$data->id) }}',
+           columns: [
+                { data: 'name', name: 'name' },
+                { data: 'percent', name: 'percent' },
+                { data: 'fixed', name:'fixed' },
+                { data: 'percent_customer', name: 'percent_customer' },
+                { data: 'fixed_customer', name:'fixed_customer' },
+                { data: 'action', name: 'action' },
+            ],
+            language : {
+                processing: '<img src="{{asset('assets/images/'.$gs->admin_loader)}}">'
+            }
+        });
+
+        $(function() {
+        $(".btn-area").append('<div class="col-sm-12 col-md-4 pr-3 text-right">'+
+            '<button class="btn btn-primary"  data-id="'+'{{$plan->id}}'+'" onclick="createglobalplan(\''+'{{$plan->id}}'+'\')" ><i class="fas fa-plus"></i> {{__('Add New Plan')}} </button>'+
+        '</a>'+
+        '</div>');
+    });
+
+
+        function getDetails (id=null)
+        {
+            if (id) {
+                var url = "{{url('admin/user/pricingplan/edit')}}"+'/'+id
+                $.get(url,function (res) {
+                  if(res == 'empty'){
+                    $('.list-group').html('<p>@lang('No details found!')</p>')
+                  }else{
+                    $('.list-group').html(res)
+                  }
+                });
+            }
+
+              $('#modal-success').modal('show')
+        }
+
+        function createDetails(id)
+        {
+                var url = "{{url('admin/user/pricingplan/create')}}"+'/'+'{{$data->id}}'+'/'+`${id}`
+                console.log(url);
+                $.get(url,function (res) {
+                  if(res == 'empty'){
+                    $('.list-group').html('<p>@lang('No details found!')</p>')
+                  }else{
+                    $('.list-group').html(res)
+                  }
+                });
+                $('#modal-success').modal('show')
+        }
+        $('.closed').click(function() {
+            $('#modal-success').modal('hide');
+        });
+
+        function createglobalplan(id)
+        {
+                var url = "{{url('admin/user/pricingplancreate')}}"+'/'+`${id}`
+                console.log(url);
+                $.get(url,function (res) {
+                  if(res == 'empty'){
+                    $('.list-group').html('<p>@lang('No details found!')</p>')
+                  }else{
+                    $('.list-group').html(res)
+                  }
+                });
+                $('#modal-success').modal('show')
+        }
+        $('.closed').click(function() {
+            $('#modal-success').modal('hide');
+        });
 </script>
 
 @endsection
