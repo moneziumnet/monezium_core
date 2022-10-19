a8218648cac9d99aa179e25656958903bea89f16
milandev128 <talent111dev@gmail.com>
2022-10-19 18:18:10 +0900

    [fix 508] display no wallet alert in checkout page

--- project/resources/views/user/merchant/checkout/link.blade.php
+++ project/resources/views/user/merchant/checkout/link.blade.php
@@ -37,36 +37,41 @@
                 <div class="row row-cards">
                     <div class="col-12">
                         <div class="card p-5">
-                                @includeIf('includes.flash')
-                                <form action="{{route('user.merchant.checkout.link_pay', $checkout->ref_id)}}" method="GET" id="pay_form" enctype="multipart/form-data">
-                                    @csrf
-                                    <div class="text-center">
-                                        <i  class="fas fa-info-circle fa-3x text-primary mb-2"></i>
-                                        <div class="col">
-                                            <h2>{{$checkout->name}}
-                                            </h2>
-                                            <div class="page-pretitle">
-                                            {{$checkout->description}}
-                                            </div>
+                            @includeIf('includes.flash')
+                            <form action="{{route('user.merchant.checkout.link_pay', $checkout->ref_id)}}" method="GET" id="pay_form" enctype="multipart/form-data">
+                                @csrf
+                                <div class="text-center">
+                                    <i  class="fas fa-info-circle fa-3x text-primary mb-2"></i>
+                                    <div class="col">
+                                        <h2>{{$checkout->name}}
+                                        </h2>
+                                        <div class="page-pretitle">
+                                        {{$checkout->description}}
                                         </div>
-                                        <h3></h3>
                                     </div>
-                                    <div class="form-group mb-3 mt-3">
-                                        <label class="form-label required">{{__('Amount')}}({{$checkout->currency->code}})</label>
-                                        @if ($checkout->amount == 0)
-                                        <input name="amount" id="amount" class="form-control" autocomplete="off"  type="text" value="{{ $checkout->amount }}" required>
-                                        @else
-                                        <input name="amount" id="amount" class="form-control" autocomplete="off"  type="text" value="{{ $checkout->amount }}" readonly required>
-                                        @endif
-                                    </div>
-                                    <input type="hidden" name="id" value="{{$checkout->id}}">
-                                    <div class="form-footer">
-                                        @foreach($cryptolist as $currency)
+                                    <h3></h3>
+                                </div>
+                                <div class="form-group mb-3 mt-3">
+                                    <label class="form-label required">{{__('Amount')}}({{$checkout->currency->code}})</label>
+                                    @if ($checkout->amount == 0)
+                                    <input name="amount" id="amount" class="form-control" autocomplete="off"  type="text" value="{{ $checkout->amount }}" required>
+                                    @else
+                                    <input name="amount" id="amount" class="form-control" autocomplete="off"  type="text" value="{{ $checkout->amount }}" readonly required>
+                                    @endif
+                                </div>
+                                <input type="hidden" name="id" value="{{$checkout->id}}">
+                                <div class="form-footer">
+                                    @foreach($cryptolist as $currency)
                                         <button type="submit" id="submit" name="link_pay_submit" value="{{$currency->id}}" class="btn btn-primary w-100">{{$currency->curr_name}} - {{$currency->code}}</button>
                                         <h3></h3>
                                     @endforeach
-                                    </div>
-                                </form>
+                                    @if (count($cryptolist) == 0)
+                                        <div class="alert alert-danger" role="alert">
+                                            This checkout's owner doesn't have any crypto wallet.
+                                        </div>
+                                    @endif
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
