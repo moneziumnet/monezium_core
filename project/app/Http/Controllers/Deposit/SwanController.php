08c666bc9b519da6005ce29d3e91c68ec05f9a5f
WHdev711 <appc31058@gmail.com>
2022-10-25 20:25:57 +0300

    Update SwanController.php

--- project/app/Http/Controllers/Deposit/SwanController.php
+++ project/app/Http/Controllers/Deposit/SwanController.php
@@ -70,11 +70,9 @@ class SwanController extends Controller
         try {
             $redirect_url = url()->previous() == url('/user/dashboard') ? url('/user/dashboard') : url('/admin/dashboard');
             if(!isset($user->company_name)) {
-                $country = Country::findOrFail($user->country);
                 $body = '{"query":"mutation MyMutation($input: OnboardIndividualAccountHolderInput) {\\n  onboardIndividualAccountHolder(\\n    input: $input\\n  ) \\n  {\\n    ... on OnboardIndividualAccountHolderSuccessPayload {\\n      __typename\\n      onboarding {\\n        id\\n        onboardingUrl\\n        redirectUrl\\n      }\\n    }\\n    ... on ForbiddenRejection {\\n      __typename\\n      message\\n    }\\n  }\\n}\\n","variables":{"input":{"email":"'.$user->email.'","redirectUrl": "'.$redirect_url.'"}}}';
             }
             else {
-                $country = Country::findOrFail($user->company_country);
                 $body = '{"query":"mutation MyMutation($input: OnboardCompanyAccountHolderInput) {\\n  onboardCompanyAccountHolder(\\n    input: $input\\n  ) {\\n    ... on OnboardCompanyAccountHolderSuccessPayload {\\n      __typename\\n      onboarding {\\n        onboardingUrl\\n      }\\n    }\\n    ... on BadRequestRejection {\\n      __typename\\n      message\\n    }\\n    ... on ForbiddenRejection {\\n      __typename\\n      message\\n    }\\n    ... on ValidationRejection {\\n      __typename\\n      message\\n    }\\n  }\\n}","variables":{"input":{"email":"'.$user->email.'","redirectUrl": "'.$redirect_url.'"}}}';
             }
             $headers = [
