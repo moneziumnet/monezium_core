a41dc0614de87a88f92886683d34abeafac65fb7
WHdev711 <appc31058@gmail.com>
2022-11-09 18:09:19 +0300

    Update index.blade.php

--- project/resources/views/user/security/index.blade.php
+++ project/resources/views/user/security/index.blade.php
@@ -21,26 +21,12 @@
     <div class="container-xl">
         <div class="row">
             <div class="col-lg-12 col-md-12">
-                {{-- @if(Auth::user()->twofa) --}}
-                    {{-- <div class="card border-0 shadow-sm">
-                        <div class="card-header">
-                            <h5 class="card-title text-dark text-center">@lang('Two Factor Authenticator')</h5>
-                        </div>
-                        <div class="card-body">
-                            <div class="form-group mx-auto text-center">
-                                <a href="javascript:void(0)"  class="btn w-100 btn-md btn--danger" data-bs-toggle="modal" data-bs-target="#disableModal">
-                                    @lang('Disable Two Factor Authenticator')</a>
-                            </div>
-                        </div>
-                    </div> --}}
-                {{-- @else --}}
                     <div class="card border-0 shadow-sm">
                         <div class="card-header">
                             <h5 class="card-title text-center">@lang('Two Factor Authenticator')</h5>
                         </div>
                         <div class="card-body">
                             @includeIf('includes.flash')
-                            {{-- <form action="{{route('send.money.store-two-auth')}}" method="POST" enctype="multipart/form-data"> --}}
                             <form action="{{route('user.securityform')}}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 <div class="form-group mb-3 mt-3">
@@ -115,60 +101,8 @@
                             </form>
                         </div>
                     </div>
-                {{-- @endif --}}
             </div>
         </div>
-
-        <!--Enable Modal -->
-        {{-- <div id="enableModal" class="modal modal-blur fade" role="dialog" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog ">
-                <!-- Modal content-->
-                <div class="modal-content ">
-                    <div class="modal-header">
-                        <h4 class="modal-title">@lang('Verify Your Otp')</h4>
-                    </div>
-                    <form action="{{route('user.createTwoFactor')}}" method="POST">
-                        @csrf
-                        <div class="modal-body ">
-                            <div class="form-group">
-                                <input type="hidden" name="key" value="{{$secret}}">
-                                <input type="text" class="form-control" name="code" placeholder="@lang('Enter Google Authenticator Code')">
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">@lang('close')</button>
-                            <button type="submit" class="btn btn-success">@lang('verify')</button>
-                        </div>
-                    </form>
-                </div>
-
-            </div>
-        </div> --}}
-
-        <!--Disable Modal -->
-        {{-- <div id="disableModal" class="modal modal-blur fade" role="dialog" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog">
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">@lang('Verify Your Otp Disable')</h4>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    </div>
-                    <form action="{{route('user.disableTwoFactor')}}" method="POST">
-                        @csrf
-                        <div class="modal-body">
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="code" placeholder="@lang('Enter Google Authenticator Code')">
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">@lang('Close')</button>
-                            <button type="submit" class="btn btn-success">@lang('Verify')</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div> --}}
     </div>
 </div>
 
