f6d9ab97f856ea0d9757ef806bb379c83c6cf1e4
aca dev <violetfocus0618@gmail.com>
2022-12-12 14:24:50 +0100

    create model and table

---
+++ project/database/migrations/tenant/2022_10_03_200418_create_kyc_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kyc_requests', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->mediumText('kyc_info')->nullable();
+            $table->timestamp('request_date')->nullable();
+            $table->timestamp('submitted_date')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kyc_requests');
+    }
+};
