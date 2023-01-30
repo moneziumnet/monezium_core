c4c5ec2474737ec1996dd6dda3562e995d49a2ca
Aca Dev <violetfocus0618@gmail.com>
2023-01-30 12:00:23 +0300

    [F-711] login history : update getIp by global

--- project/app/Http/Controllers/User/RegisterController.php
+++ project/app/Http/Controllers/User/RegisterController.php
@@ -310,7 +310,7 @@ class RegisterController extends Controller
             $activity = new LoginActivity();
             $activity->subject = 'User Register Successfully.';
             $activity->url = facade_request::fullUrl();
-            $activity->ip = facade_request::getClientIp();
+            $activity->ip = facade_request::getClientIp(true);
             $activity->agent = facade_request::header('user-agent');
             $activity->user_id = $user->id;
             $activity->save();
