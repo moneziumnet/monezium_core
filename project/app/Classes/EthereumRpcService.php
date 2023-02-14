bb2d0e67ed2c8eb6d772ef20b64f7cf3d5334a73
Aca Dev <violetfocus0618@gmail.com>
2023-02-14 16:22:09 +0300

    Update EthereumRpcService.php

--- project/app/Classes/EthereumRpcService.php
+++ project/app/Classes/EthereumRpcService.php
@@ -137,7 +137,7 @@ class EthereumRpcService
         try {
             $response = $client->request('POST', $link, ["headers" => $headers, "body" => json_encode($body), 'connect_timeout' => 0.5]);
             $res = json_decode($response->getBody());
-            Log::Info($res);
+            // Log::info($res);
         } catch (\Throwable $th) {
                 return 'error';
         }
