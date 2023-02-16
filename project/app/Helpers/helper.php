3e2a7d806444f4d04fb0c2fb7b038d154d9b8aee
Aca Dev <violetfocus0618@gmail.com>
2023-02-16 22:50:33 +0300

    [F-801] Telegram bot integration : admin notification

--- project/app/Helpers/helper.php
+++ project/app/Helpers/helper.php
@@ -1280,6 +1280,7 @@ if (!function_exists('send_staff_telegram')) {
                 send_message_telegram($telegram->user_id, $message);
             }
         }
+        send_message_telegram(0, $message);
     }
 }
 
