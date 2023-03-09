e1c7564c9cc25cb7592e9fd4f09d83566dc18ae3
Aca Dev <violetfocus0618@gmail.com>
2023-03-09 16:21:54 +0300

    [B-1001] Email format : test created_at

--- project/app/Handler/OpenpaydResponse.php
+++ project/app/Handler/OpenpaydResponse.php
@@ -69,6 +69,8 @@ class OpenpaydResponse implements RespondsToWebhook
                     send_whatsapp($user->id, 'Bank has been deposited by '.$obj->senderName."\n Amount is ".$currency->symbol.$obj->amount->value."\n Payment Gateway : Openpayd"."\n Transaction ID : ".$obj->transactionId."\nPlease check more details to click this url\n".route('user.depositbank.index'));
                     send_telegram($user->id, 'Bank has been deposited by '.$obj->senderName."\n Amount is ".$currency->symbol.$obj->amount->value."\n Payment Gateway : Openpayd"."\n Transaction ID : ".$obj->transactionId."\nPlease check more details to click this url\n".route('user.depositbank.index'));
                     send_staff_telegram('Bank has been deposited by '.$obj->senderName."\n Amount is ".$currency->symbol.$obj->amount->value."\n Payment Gateway : Openpayd"."\n Transaction ID : ".$obj->transactionId."\nPlease check more details to click this url\n".route('admin.deposits.bank.index'), 'Deposit Bank');
+                    mailSend('deposit_request',['amount'=>$new_deposit->amount, 'curr' => ($currency ? $currency->code : ' '), 'date_time'=>$new_deposit->created_at ,'type' => 'Bank', 'method'=> $subbank->name ], $user);
+
 
                 }
             }
@@ -95,6 +97,7 @@ class OpenpaydResponse implements RespondsToWebhook
                     $new_deposit['details'] = $obj->transactionReference;
                     $new_deposit['sub_bank_id'] = isset($subbank_id) ? $subbank_id : null;
                     $new_deposit->save();
+                    mailSend('deposit_request',['amount'=>$new_deposit->amount, 'curr' => ($currency ? $currency->code : ' '), 'date_time'=>$new_deposit->created_at ,'type' => 'Bank', 'method'=> $subbank->name ], $user);
                     send_notification($user->id, 'Bank has been deposited by '.$obj->senderName.'. Please check.', route('admin.deposits.bank.index'));
                     send_whatsapp($user->id, 'Bank has been deposited by '.$obj->senderName."\n Amount is ".$currency->symbol.$obj->amount->value."\n Payment Gateway : Openpayd"."\n Transaction ID : ".$obj->transactionId."\nPlease check more details to click this url\n".route('user.depositbank.index'));
                     send_telegram($user->id, 'Bank has been deposited by '.$obj->senderName."\n Amount is ".$currency->symbol.$obj->amount->value."\n Payment Gateway : Openpayd"."\n Transaction ID : ".$obj->transactionId."\nPlease check more details to click this url\n".route('user.depositbank.index'));
