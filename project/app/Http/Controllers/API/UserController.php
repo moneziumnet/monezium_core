25c2eacca3a2433be0e057163e81d2f3c08ce0ae
Aca Dev <violetfocus0618@gmail.com>
2023-03-15 10:28:18 +0300

    [F API] add wallet, crypto wallet, get gateway

--- project/app/Http/Controllers/API/UserController.php
+++ project/app/Http/Controllers/API/UserController.php
@@ -2,28 +2,39 @@
 
 namespace App\Http\Controllers\API;
 
-use Session;
+use PDF;
+use Auth;
 use Validator;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\ValidationException;
+use Carbon\Carbon;
 use App\Models\User;
-use App\Models\UserApiCred;
-use App\Models\Wallet;
-use App\Models\Transaction;
-use App\Models\BankPlan;
-use App\Models\AdminUserConversation;
-use App\Models\Generalsetting;
-use App\Models\Notification;
-use App\Models\Currency;
+use App\Models\Order;
 use App\Models\Charge;
-use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
-
-use App\Classes\GoogleAuthenticator;
-use PHPMailer\PHPMailer\PHPMailer;
-use App\Http\Controllers\Controller;
+use App\Models\Wallet;
+use App\Traits\Payout;
+use App\Models\Currency;
+use App\Models\UserLoan;
+use App\Models\SubInsBank;
+use App\Models\BankAccount;
+use App\Models\BankGateway;
+use App\Models\Transaction;
+use App\Models\KycRequest;
+use App\Models\LoginActivity;
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Models\Generalsetting;
+use App\Models\InstallmentLog;
+use App\Exports\ExportTransaction;
+use App\Classes\GoogleAuthenticator;
+use App\Http\Controllers\Controller;
+use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Carbon as Carbontime;
+use PhpOffice\PhpSpreadsheet\Calculation\DateTimeExcel\Current;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\Console\Completion\Suggestion;
 
 class UserController extends Controller
 {
@@ -197,19 +208,166 @@ class UserController extends Controller
     public function dashboard(Request $request)
     {
         try {
-            $user_id = Auth::user()->id;
-            $data['user'] = User::whereId($user_id)->first();
-            $wallets = Wallet::where('user_id',$user_id)->where('user_type',1)->with('currency')->get();
+            wallet_monthly_fee(auth()->id());
+            $gs = Generalsetting::first();
+            $data['user'] = Auth::user();
+            $wallets = Wallet::where('user_id',auth()->id())->where('user_type',1)->where('wallet_type', 1)->with('currency')->get();
             $data['wallets'] = $wallets;
-            $data['transactions'] = Transaction::whereUserId($user_id)->orderBy('id','desc')->limit(5)->get();
+            $data['cryptowallets'] = Wallet::where('user_id',auth()->id())->where('user_type',1)->where('wallet_type', 8)->with('currency')->get();
+            $data['transactions'] = Transaction::whereUserId(auth()->id())->orderBy('id','desc')->limit(5)->get();
+            $data['bankaccountlist'] = BankAccount::whereUserId(auth()->id())->get();
+            $data['currencies'] = Currency::where('type', 1)->where('status', 1)->get();
+            $data['crypto_currencies'] = Currency::where('type', 2)->where('status', 1)->get();
+            $data['subbank'] = SubInsBank::wherestatus(1)->get();
+            $data['kyc_request_id'] = 0;
+            $data['kyc_request_status'] = 4;
+            $kycrequest = KycRequest::where('user_id', auth()->id())->whereIn('status', [0, 2])->first();
+            if($kycrequest){
+                $data['kyc_request_status'] = $kycrequest->status;
+                $data['kyc_request_id'] = $kycrequest->id;
+            }
+            else {
+                $kycrequest = KycRequest::where('user_id', auth()->id())->whereIn('status', [1, 3])->first();
+                if ($kycrequest) {
+                    $data['kyc_request_status'] = $kycrequest->status;
+                    $data['kyc_request_id'] = $kycrequest->id;
+                }
+            }
+
             foreach ($data['transactions'] as $key => $transaction) {
                 $transaction->currency = Currency::whereId($transaction->currency_id)->first();
             }
-            $data['userBalance'] = userBalance($user_id);
+            $data['userBalance'] = userBalance(auth()->id());
             return response()->json(['status' => '200', 'error_code' => '0', 'message' => 'success', 'data' => $data]);
         } catch (\Throwable $th) {
             return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'Something invalid.']);
         }
+    }
+
+    public function wallet_create (Request $request) {
+        $check =  Wallet::where('user_id', $request->user_id)->where('wallet_type', 1)->where('currency_id', $request->currency_id)->first();
+        if($check){
+            return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'This wallet already exist.']);
+        }
+        $gs = Generalsetting::first();
+        $user_wallet = new Wallet();
+        $user_wallet->user_id = $request->user_id;
+        $user_wallet->user_type = 1;
+        $user_wallet->currency_id = $request->currency_id;
+        $user_wallet->balance = 0;
+        $user_wallet->wallet_type = 1;
+        $user_wallet->wallet_no =$gs->wallet_no_prefix. date('ydis') . random_int(100000, 999999);
+        $user_wallet->created_at = date('Y-m-d H:i:s');
+        $user_wallet->updated_at = date('Y-m-d H:i:s');
+        $user_wallet->save();
+
+        $user =  User::findOrFail($request->user_id);
+        $chargefee = Charge::where('slug', 'account-open')->where('plan_id', $user->bank_plan_id)->where('user_id', $user->id)->first();
+        if(!$chargefee) {
+            $chargefee = Charge::where('slug', 'account-open')->where('plan_id', $user->bank_plan_id)->where('user_id', 0)->first();
+        }
+
+        $trans = new Transaction();
+        $trans->trnx = str_rand();
+        $trans->user_id     = $request->user_id;
+        $trans->user_type   = 1;
+        $trans->currency_id = defaultCurr();
+        $trans->amount      = 0;
+
+        $trans_wallet = get_wallet($request->user_id, defaultCurr(), 1);
+
+        $trans->wallet_id   = isset($trans_wallet) ? $trans_wallet->id : null;
+        $trans->charge      = $chargefee->data->fixed_charge;
+        $trans->type        = '-';
+        $trans->remark      = 'account-open';
+        $trans->details     = trans('Wallet Create');
+        $trans->data        = '{"sender":"'.($user->company_name ?? $user->name).'", "receiver":"'.$gs->disqus.'"}';
+        $trans->save();
+
+
+        $def_currency = Currency::findOrFail(defaultCurr());
+        mailSend('wallet_create',['amount'=>$trans->charge, 'trnx'=> $trans->trnx,'curr' => $def_currency->code, 'type' => 'Current', 'date_time'=> dateFormat($trans->created_at)], $user);
+
+        user_wallet_decrement($user->id, defaultCurr(), $chargefee->data->fixed_charge, 1);
+        user_wallet_increment(0, defaultCurr(), $chargefee->data->fixed_charge, 9);
+        return response()->json(['status' => '200', 'error_code' => '0', 'message' => 'You have created new Wallet successfully.']);
+    }
+
+
+    public function crypto_wallet_create(Request $request) {
+        $check =  Wallet::where('user_id', $request->user_id)->where('wallet_type', 8)->where('currency_id', $request->crypto_currency_id)->first();
+        if($check){
+            return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'This wallet already exist.']);
+        }
+        $currency = Currency::findOrFail($request->crypto_currency_id);
+        if ($currency->code == 'BTC') {
+            $keyword = str_rand();
+            $address = RPC_BTC_Create('createwallet',[$keyword]);
+        }
+        else if ($currency->code == 'ETH'){
+            $keyword = str_rand(6);
+            $address = RPC_ETH('personal_newAccount',[$keyword]);
+        } else {
+            $eth_currency = Currency::where('code', 'ETH')->first();
+            $eth_wallet = Wallet::where('user_id', $request->user_id)->where('wallet_type', 8)->where('currency_id', $eth_currency->id)->first();
+            if (!$eth_wallet) {
+                return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'You have to create Eth Crypto wallet firstly before create ERC20 token wallet.']);
+            }
+            $address = $eth_wallet->wallet_no;
+            $keyword = $eth_wallet->keyword;
+        }
+        if ($address == 'error') {
+            return response()->json(['status' => '401', 'error_code' => '0', 'message' => 'You can not create this wallet because there is some issue in crypto node.']);
+        }
+
+        $gs = Generalsetting::first();
+        $user_wallet = new Wallet();
+        $user_wallet->user_id = $request->user_id;
+        $user_wallet->user_type = 1;
+        $user_wallet->currency_id = $request->crypto_currency_id;
+        $user_wallet->balance = 0;
+        $user_wallet->wallet_type = 8;
+        $user_wallet->wallet_no = $address;
+        $user_wallet->keyword = $keyword;
+        $user_wallet->created_at = date('Y-m-d H:i:s');
+        $user_wallet->updated_at = date('Y-m-d H:i:s');
+        $user_wallet->save();
+
+
+        $user =  User::findOrFail($request->user_id);
+        $chargefee = Charge::where('slug', 'account-open')->where('plan_id', $user->bank_plan_id)->where('user_id', $user->id)->first();
+        if(!$chargefee) {
+            $chargefee = Charge::where('slug', 'account-open')->where('plan_id', $user->bank_plan_id)->where('user_id', 0)->first();
+        }
+        $trans = new Transaction();
+        $trans->trnx = str_rand();
+        $trans->user_id     = $request->user_id;
+        $trans->user_type   = 1;
+        $trans->currency_id = defaultCurr();
+        $trans->amount      = 0;
+
+        $trans_wallet = get_wallet($request->user_id, defaultCurr(), 1);
+
+        $trans->wallet_id   = isset($trans_wallet) ? $trans_wallet->id : null;
+        $trans->charge      = $chargefee->data->fixed_charge;
+        $trans->type        = '-';
+        $trans->remark      = 'account-open';
+        $trans->details     = trans('Wallet Create');
+        $trans->data        = '{"sender":"'.($user->company_name ?? $user->name).'", "receiver":"'.$gs->disqus.'"}';
+        $trans->save();
+
+        $def_currency = Currency::findOrFail(defaultCurr());
+        mailSend('wallet_create',['amount'=>$trans->charge, 'trnx'=> $trans->trnx,'curr' => $def_currency->code, 'type' => 'Crypto', 'date_time'=> dateFormat($trans->created_at)], $user);
+
+        user_wallet_decrement($user->id, defaultCurr(), $chargefee->data->fixed_charge, 1);
+        user_wallet_increment(0, defaultCurr(), $chargefee->data->fixed_charge, 9);
+
+        return response()->json(['status' => '200', 'error_code' => '0', 'message' => 'You have created new Wallet successfully.']);
+    }
+
+    public function gateway(Request $request) {
+        $bankgateway = BankGateway::where('subbank_id', $request->id)->first();
+        return response()->json(['status' => '200', 'error_code' => '0', 'message' => 'success', 'data' => $bankgateway]);
     }
 
     public function packages(Request $request)
