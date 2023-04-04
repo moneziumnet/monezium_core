3ce0b7fa2649437c2ab41a2c023c3c2e1d3393c4
Aca Dev <violetfocus0618@gmail.com>
2023-04-04 09:22:08 +0200

    Update header.blade.php

--- project/resources/views/includes/user/header.blade.php
+++ project/resources/views/includes/user/header.blade.php
@@ -1,5 +1,5 @@
 <header class="navbar navbar-expand-xl navbar-light d-print-none">
-  <div class="container-xl">
+  <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu">
       <span class="navbar-toggler-icon"></span>
     </button>
