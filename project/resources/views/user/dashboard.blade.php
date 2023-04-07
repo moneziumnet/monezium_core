125c99c417f988efa02ee315d8222c9a6c6029b4
Aca Dev <violetfocus0618@gmail.com>
2023-04-07 16:20:16 +0300

    [B-1401] change default theme to Paysa : change color
     change color of crypto wallet address, link of transaction in customer dashboard

--- project/resources/views/user/dashboard.blade.php
+++ project/resources/views/user/dashboard.blade.php
@@ -297,7 +297,7 @@
                           <div class="card-body">
                               <div class="row">
                                 <div class="col h3 m-0 text-uppercase">{{ amount(Crypto_Balance($item->user_id, $item->currency_id), 2)}}  {{$item->currency->code}}</div>
-                                <a class="col-auto btn-icon-clipboard copy" data-clipboard-text="{{$item->wallet_no}}" title="Copy"><i class="fa fa-copy text-xs"></i></a>
+                                <a class="col-auto btn-icon-clipboard copy tx-color" data-clipboard-text="{{$item->wallet_no}}" title="Copy"><i class="fa fa-copy text-xs"></i></a>
                               </div>
                               <div class="h4 m-0"> {{str_dis($item->wallet_no)}}</div>
                               <div class="text-muted"> ({{ amount(Crypto_Balance_Fiat($item->user_id, $item->currency_id), 1)}}  {{$currency->code}})</div>
@@ -460,7 +460,7 @@
                 <div class="card-actions">
                   <a href="{{route('user.transaction')}}" >
                     
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right tx-color" viewBox="0 0 16 16">
                       <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
                     </svg>
                   </a>
