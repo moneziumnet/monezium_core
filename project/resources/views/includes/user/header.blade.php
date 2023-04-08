93c868f6e4f5b9263baad158627d23ddda10df46
Aca Dev <violetfocus0618@gmail.com>
2023-04-08 19:24:32 +0300

    [F-1403] Add Notification Icon in header

--- project/resources/views/includes/user/header.blade.php
+++ project/resources/views/includes/user/header.blade.php
@@ -35,7 +35,7 @@
           </div>
         </form>
       </div>
-      <a href="?theme=dark" class="nav-link px-0 hide-theme-dark" title="Enable dark mode" data-bs-toggle="tooltip" data-bs-placement="bottom">
+      <a href="?theme=dark" class="nav-link px-0 hide-theme-dark " title="Enable dark mode" data-bs-toggle="tooltip" data-bs-placement="bottom">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
@@ -48,6 +48,51 @@
           <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
         </svg>
       </a>
+      @php
+        $notifications = DB::table('action_notifications')->where('user_id', auth()->id())->latest()->limit(5)->get();
+      @endphp   
+      <div class="nav-item dropdown me-3">
+        <a href="#" class="nav-link px-0" data-bs-toggle="dropdown" tabindex="-1" aria-label="Show notifications" aria-expanded="false">
+          <!-- Download SVG icon from http://tabler-icons.io/i/bell -->
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6"></path><path d="M9 17v1a3 3 0 0 0 6 0v-1"></path></svg>
+          @if ($notifications->count() > 0)
+          <span class="badge bg-red"></span>
+          @endif
+        </a>
+        <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-end dropdown-menu-card">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">@lang('Last Notications')</h3>
+            </div>
+            <div class="list-group list-group-flush list-group-hoverable">
+              @if ($notifications->count() == 0)
+              <div class="list-group-item">
+                <div class="row align-items-center">
+                  <div class="col-md-12 text-center">
+                    <div class="h3 d-block text-muted text-truncate mt-n1 ms-5 me-5">
+                      @lang('No Notications')
+                    </div>
+                  </div>
+                </div>
+              </div>
+              @else
+                @foreach ($notifications as $key => $item )
+                  <div class="list-group-item">
+                    <div class="row align-items-center">
+                      <div class="col text-truncate">
+                        <a href="#" class="text-body d-block">@lang("Notification") {{__($key + 1)}}</a>
+                        <div class="d-block text-muted text-truncate mt-n1">
+                          {{__(substr($item->description, 0, 50))}}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                @endforeach
+              @endif
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="nav-item dropdown">
         <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
           <span class="avatar avatar-sm" style="background-image: url({{ auth()->user()->photo ? asset('assets/images/'.auth()->user()->photo) : asset('assets/user/img/user.jpg')}})"></span>
