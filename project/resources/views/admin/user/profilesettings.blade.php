56ec79acbdb28998b06961d7334a92226fbc8535
Aca Dev <violetfocus0618@gmail.com>
2023-05-08 00:01:51 +0300

    [F-1703-3] Move supervisor set, user type  from information page to Setting page

--- project/resources/views/admin/user/profilesettings.blade.php
+++ project/resources/views/admin/user/profilesettings.blade.php
@@ -74,6 +74,29 @@
                   </div>
               </div>
 
+              <div class="form-group">
+                <label for="inp-name">{{ __('Select Supervisor') }}</label>
+
+                <select class="form-control" name="referral_id" id="referral_id" >
+                  <option value="0">{{ __('Select Supervisor') }}</option>
+                  @foreach ($user_list as $item)
+                    @if(check_user_type_by_id(4, $item->id))
+                      <option value="{{$item->id}}" @if($item->id == $data->referral_id) selected @endif>{{$item->company_name ?? $item->name}}</option>
+                    @endif
+                  @endforeach
+              </select>
+              </div>
+
+              <div class="form-group">
+                <label for="inp-name">{{ __('Type') }}</label>
+
+                <select class="select mb-3" name="user_type[]" multiple id="user_type">
+                  {{-- <option value="">{{ __('Select Customer Type') }}</option> --}}
+                  @foreach(DB::table('customer_types')->orderBy('type_name','asc')->get() as $c_type)
+                    <option value="{{ $c_type->id }}" @if(in_array($c_type->id, $userType)) selected @endif>{{ $c_type->type_name }}</option>
+                  @endforeach
+                </select>
+              </div>
               <button type="submit" id="submit-btn" class="btn btn-primary w-100">{{ __('Submit') }}</button>
             </form>
           </div>
@@ -111,4 +134,112 @@
 </script>
 @endsection
 
+@section('styles')
+<style type="text/css">
+  .ms-options-wrap,
+  .ms-options-wrap * {
+    box-sizing: border-box;
+  }
 
+  .ms-options ul li {
+    list-style: none;
+    margin-left: -40px;
+  }
+
+  .ms-options-wrap>button:focus,
+  .ms-options-wrap>button {
+    position: relative;
+    width: 100%;
+    text-align: left;
+    border: 1px solid #d1d3e2;
+    background-color: #fff;
+    padding: 5px 20px 5px 5px;
+    margin-top: 1px;
+    font-size: 13px;
+    color: #6e707e;
+    outline: none;
+    white-space: nowrap;
+  }
+
+  .ms-options-wrap>button:after {
+    content: ' ';
+    height: 0;
+    position: absolute;
+    top: 50%;
+    right: 5px;
+    width: 0;
+    border: 6px solid rgba(0, 0, 0, 0);
+    border-top-color: #999;
+    margin-top: -3px;
+  }
+
+  .ms-options-wrap>.ms-options {
+    position: absolute;
+    left: 0;
+    width: 100%;
+    margin-top: 1px;
+    margin-bottom: 20px;
+    background: white;
+    z-index: 2000;
+    border: 1px solid #d1d3e2;
+    text-align: left;
+  }
+
+  .ms-options-wrap>.ms-options>.ms-search input {
+    width: 100%;
+    padding: 4px 5px;
+    border: none;
+    border-bottom: 1px groove;
+    outline: none;
+  }
+
+  .ms-options-wrap>.ms-options .ms-selectall {
+    display: inline-block;
+    font-size: .9em;
+    text-transform: lowercase;
+    text-decoration: none;
+  }
+
+  .ms-options-wrap>.ms-options .ms-selectall:hover {
+    text-decoration: underline;
+  }
+
+  .ms-options-wrap>.ms-options>.ms-selectall.global {
+    margin: 4px 5px;
+  }
+
+  .ms-options-wrap>.ms-options>ul>li.optgroup {
+    padding: 5px;
+  }
+
+  .ms-options-wrap>.ms-options>ul>li.optgroup+li.optgroup {
+    border-top: 1px solid #aaa;
+  }
+
+  .ms-options-wrap>.ms-options>ul>li.optgroup .label {
+    display: block;
+    padding: 5px 0 0 0;
+    font-weight: bold;
+  }
+
+  .ms-options-wrap>.ms-options>ul label {
+    position: relative;
+    display: inline-block;
+    width: 100%;
+    padding: 2px 3px;
+    margin: 1px 0;
+  }
+
+  .ms-options-wrap>.ms-options>ul li.selected label,
+  .ms-options-wrap>.ms-options>ul label:hover {
+    background-color: #efefef;
+  }
+
+  .ms-options-wrap>.ms-options>ul input[type="checkbox"] {
+    margin-right: 5px;
+    position: absolute;
+    left: 4px;
+    top: 7px;
+  }
+</style>
+@endsection
