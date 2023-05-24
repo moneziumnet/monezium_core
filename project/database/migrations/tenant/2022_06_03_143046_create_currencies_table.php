f1c48248becf6682dea0066097f26043dbbf1efc
Aca Dev <violetfocus0618@gmail.com>
2023-05-24 11:12:11 +0300

    [F-1801] update datatable

--- project/database/migrations/tenant/2022_06_03_143046_create_currencies_table.php
+++ project/database/migrations/tenant/2022_06_03_143046_create_currencies_table.php
@@ -23,6 +23,7 @@ class CreateCurrenciesTable extends Migration
             $table->string('address')->nullable();
             $table->string('keyword')->nullable();
             $table->unsignedInteger('cryptodecimal')->default(18);
+            $table->string('network')->nullable();
             $table->unsignedInteger('status')->default(1)->comment('1 => active, 0 => inactive');
             $table->unsignedDecimal('rate', 20, 10);
             $table->timestamps();
