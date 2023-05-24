9b0db90921b2a865a9decee670dba58dcb0c14af
Aca Dev <violetfocus0618@gmail.com>
2023-05-24 11:23:43 +0300

    Update 2022_06_03_143046_create_currencies_table.php

--- project/database/migrations/tenant/2022_06_03_143046_create_currencies_table.php
+++ project/database/migrations/tenant/2022_06_03_143046_create_currencies_table.php
@@ -21,9 +21,8 @@ class CreateCurrenciesTable extends Migration
             $table->string('curr_name');
             $table->unsignedInteger('type')->default(1)->comment('1 => fiat, 2 => crypto');
             $table->string('address')->nullable();
-            $table->string('keyword')->nullable();
-            $table->unsignedInteger('cryptodecimal')->default(18);
             $table->string('network')->nullable();
+            $table->unsignedInteger('cryptodecimal')->default(18);
             $table->unsignedInteger('status')->default(1)->comment('1 => active, 0 => inactive');
             $table->unsignedDecimal('rate', 20, 10);
             $table->timestamps();
